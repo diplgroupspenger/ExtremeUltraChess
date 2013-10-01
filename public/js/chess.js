d18492c5f7690a56e0731e351a4c847d0ba05e57
FlorianLangeder <flo.langeder@gmail.com>
2013-10-01 08:39:32 +0200

    Canvas drawing

--- public/js/chess.js
+++ public/js/chess.js
@@ -1,73 +1,97 @@
-var WHITE_KING = "wKing";
-var WHITE_QUEEN = "wQueen";
-var WHITE_ROOK = "wRook";
-var WHITE_BISHOP = "wBishop";
-var WHITE_KNIGHT = "wKnight";
-var WHITE_PAWN = "wPawn";
+var WHITE_KING = 4;
+var WHITE_QUEEN = 3;
+var WHITE_ROOK = 5;
+var WHITE_BISHOP = 2;
+var WHITE_KNIGHT = 1;
+var WHITE_PAWN = 0;
  
-var BLACK_KING = "bKing";
-var BLACK_QUEEN = "bQueen";
-var BLACK_ROOK = "bRook";
-var BLACK_BISHOP = "bBishop";
-var BLACK_KNIGHT = "bKnight";
-var BLACK_PAWN = "bPawn";
+var BLACK_KING = 10;
+var BLACK_QUEEN = 9;
+var BLACK_ROOK = 11;
+var BLACK_BISHOP = 8;
+var BLACK_KNIGHT = 7;
+var BLACK_PAWN = 6;
 
-var RED_KING = "rKing";
-var RED_QUEEN = "rQueen";
-var RED_ROOK = "rRook";
-var RED_BISHOP = "rBishop";
-var RED_KNIGHT = "rKnight";
-var RED_PAWN = "rPawn";
+var RED_KING = 16;
+var RED_QUEEN = 15;
+var RED_ROOK = 17;
+var RED_BISHOP = 14;
+var RED_KNIGHT = 13;
+var RED_PAWN = 12;
 
-var GREEN_KING = "gKing";
-var GREEN_QUEEN = "gQueen";
-var GREEN_ROOK = "gRook";
-var GREEN_BISHOP = "gBishop";
-var GREEN_KNIGHT = "gKnight";
-var GREEN_PAWN = "gPawn";
+var GREEN_KING = 22;
+var GREEN_QUEEN = 21;
+var GREEN_ROOK = 23;
+var GREEN_BISHOP = 20;
+var GREEN_KNIGHT = 19;
+var GREEN_PAWN = 18;
 
-var EMPTY = -1;
+var EMPTY = -2;
 
-var board = [[-1,-1,-1,WHITE_ROOK,WHITE_BISHOP,WHITE_KNIGHT,WHITE_QUEEN,WHITE_KING,WHITE_KNIGHT,WHITE_BISHOP,WHITE_ROOK,-1,-1,-1],
-             [-1,-1,-1,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,-1,-1,-1],
-             [-1,-1,-1,0,0,0,0,0,0,0,0,-1,-1,-1],
-             [RED_ROOK,RED_PAWN,0,0,0,0,0,0,0,0,0,0,GREEN_PAWN,GREEN_ROOK],
-             [RED_BISHOP,RED_PAWN,0,0,0,0,0,0,0,0,0,0,GREEN_PAWN,GREEN_BISHOP],
-             [RED_KNIGHT,RED_PAWN,0,0,0,0,0,0,0,0,0,0,GREEN_PAWN,GREEN_KNIGHT],
-             [RED_QUEEN,RED_PAWN,0,0,0,0,0,0,0,0,0,0,GREEN_PAWN,GREEN_QUEEN],
-             [RED_KING,RED_PAWN,0,0,0,0,0,0,0,0,0,0,GREEN_PAWN,GREEN_KING],
-             [RED_KNIGHT,RED_PAWN,0,0,0,0,0,0,0,0,0,0,GREEN_PAWN,GREEN_KNIGHT],
-             [RED_BISHOP,RED_PAWN,0,0,0,0,0,0,0,0,0,0,GREEN_PAWN,GREEN_BISHOP],
-             [RED_ROOK,RED_PAWN,0,0,0,0,0,0,0,0,0,0,GREEN_PAWN,GREEN_ROOK],
-             [-1,-1,-1,0,0,0,0,0,0,0,0,-1,-1,-1],
-             [-1,-1,-1,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,-1,-1,-1],
-             [-1,-1,-1,BLACK_ROOK,BLACK_BISHOP,BLACK_KNIGHT,BLACK_QUEEN,BLACK_KING,BLACK_KNIGHT,BLACK_BISHOP,BLACK_ROOK,-1,-1,-1]];
+
+var board = [[-2,-2,-2,WHITE_ROOK,WHITE_BISHOP,WHITE_KNIGHT,WHITE_QUEEN,WHITE_KING,WHITE_KNIGHT,WHITE_BISHOP,WHITE_ROOK,-2,-2,-2],
+             [-2,-2,-2,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,WHITE_PAWN,-2,-2,-2],
+             [-2,-2,-2,-1,-1,-1,-1,-1,-1,-1,-1,-2,-2,-2],
+             [RED_ROOK,RED_PAWN,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,GREEN_PAWN,GREEN_ROOK],
+             [RED_BISHOP,RED_PAWN,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,GREEN_PAWN,GREEN_BISHOP],
+             [RED_KNIGHT,RED_PAWN,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,GREEN_PAWN,GREEN_KNIGHT],
+             [RED_QUEEN,RED_PAWN,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,GREEN_PAWN,GREEN_QUEEN],
+             [RED_KING,RED_PAWN,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,GREEN_PAWN,GREEN_KING],
+             [RED_KNIGHT,RED_PAWN,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,GREEN_PAWN,GREEN_KNIGHT],
+             [RED_BISHOP,RED_PAWN,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,GREEN_PAWN,GREEN_BISHOP],
+             [RED_ROOK,RED_PAWN,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,GREEN_PAWN,GREEN_ROOK],
+             [-2,-2,-2,-1,-1,-1,-1,-1,-1,-1,-1,-2,-2,-2],
+             [-2,-2,-2,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,BLACK_PAWN,-2,-2,-2],
+             [-2,-2,-2,BLACK_ROOK,BLACK_BISHOP,BLACK_KNIGHT,BLACK_QUEEN,BLACK_KING,BLACK_KNIGHT,BLACK_BISHOP,BLACK_ROOK,-2,-2,-2]];
 
 $(function() {
-    drawBoard(board);
+    pieces = new Image();
+    pieces.onload = function () {
+        drawBoard();
+    }
+    
+    pieces.src = "img/figures.png";
+    
 });
 
-function drawBoard(board) {
-    var str = '';
-    for( var i = 0 ; i < board.length ; i++ ){
-        str += '<div class="row">';
-        for( var q = 0 ; q < board.length ; q++ ){
+function drawBoard() {
+    canvas = document.getElementById('board');
 
-            var color = getBoardColor(i, q);
+        ctx = canvas.getContext('2d');
+        BLOCK_SIZE = canvas.height / 14;
+        for( var i = 0 ; i < 14 ; i++ ){
+            for( var q = 0 ; q < 14 ; q++ ){
 
-            str += '<div class="column ' +
-            color + '">' +
-            '<div class="' + board[i][q] + '"></div>' +
-            '</div>';
+                var x = q * BLOCK_SIZE;
+                var y = i * BLOCK_SIZE;
+                if(board[i][q] != -2) {
+                    ctx.fillStyle = getBoardColor(i,q);
+                    ctx.fillRect(x,y,BLOCK_SIZE,BLOCK_SIZE);
+                    if(board[i][q] != -1)
+                        drawFigure(i,q);
+                }
 
+            }
+            
         }
-        str += '</div>';
-    }
-    $('#board').append(str);
+}
+
+function drawFigure(x,y) {
+    var figurePos = getPosFromFigure(board[x][y]);
+    ctx.drawImage(pieces,figurePos.x, figurePos.y, 50, 50, y*50, x*50, 50, 50); 
+}
+
+function getPosFromFigure(index) {
+    var y = Math.floor(index / 6);
+    var x = index - (6*y);
+
+    var position = {
+        "y": y * 50,
+        "x": x * 50
+    };
+    return position;
 }
 
 function getBoardColor(x, y) {
-    if(board[x][y] != -1)
-       return (x + y) % 2 === 0 ? 'light': 'dark';
-	
+       return (x + y) % 2 === 0 ? '#fee472': '#00B392';
 }
