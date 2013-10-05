67db261530935de171c09866a7285993ad2a8d81
FlorianLangeder <flo.langeder@gmail.com>
2013-10-05 23:46:07 +0200

    Added move sync

--- app.js
+++ app.js
@@ -4,30 +4,31 @@ var express = require('express'),
   , server = http.createServer(app)
   , io = require('socket.io').listen(server);
 
-
 io.set('transports', [ 'htmlfile', 'xhr-polling', 'jsonp-polling' ]);
-
 
 // listen for new web clients:
 server.listen(4000);
 
 app.use(express.static(__dirname+'/public'));
- 
-app.get('/', function(request, response){
-  response.sendfile(__dirname + "/index.html");
-});
- 
+  
 var activeClients = 0;
  
-io.sockets.on('connection', function(socket){clientConnect(socket)});
-
-function clientConnect(socket){
+io.sockets.on('connection',function(socket){
   activeClients +=1;
   io.sockets.emit('message', {clients:activeClients});
-  socket.on('disconnect', function(){clientDisconnect()});
-}
+  socket.on('disconnect', clientDisconnect);
+
+  socket.on('sendPosition',function(pos, figure){
+      io.sockets.emit('setPosition',pos, figure);
+  });
+  
+  socket.on('sendRemoveFigure',function(index){
+      io.sockets.emit('removeFigure',index);
+  });
+});
  
 function clientDisconnect(){
   activeClients -=1;
   io.sockets.emit('message', {clients:activeClients});
 }
+
--- public/js/chess.js
+++ public/js/chess.js
@@ -10,29 +10,48 @@ var Color = {
 }
 
 //array of the chessboard
-var board = [[-2,-2,-2,new Figure(FigureType.ROOK, Color.BLACK),new Figure(FigureType.BISHOP,Color.BLACK),new Figure(FigureType.KNIGHT,Color.BLACK),new Figure(FigureType.QUEEN,Color.BLACK),new Figure(FigureType.KING,Color.BLACK),new Figure(FigureType.KNIGHT,Color.BLACK),new Figure(FigureType.BISHOP,Color.BLACK),new Figure(FigureType.ROOK,Color.BLACK),-2,-2,-2],
+var board = [[-2,-2,-2,new Figure(FigureType.ROOK, Color.BLACK),new Figure(FigureType.KNIGHT, Color.BLACK),new Figure(FigureType.BISHOP,Color.BLACK),new Figure(FigureType.KING,Color.BLACK),new Figure(FigureType.QUEEN,Color.BLACK),new Figure(FigureType.BISHOP,Color.BLACK),new Figure(FigureType.KNIGHT,Color.BLACK),new Figure(FigureType.ROOK,Color.BLACK),-2,-2,-2],
              [-2,-2,-2,new Figure(FigureType.PAWN,Color.BLACK),new Figure(FigureType.PAWN,Color.BLACK),new Figure(FigureType.PAWN,Color.BLACK),new Figure(FigureType.PAWN,Color.BLACK),new Figure(FigureType.PAWN,Color.BLACK),new Figure(FigureType.PAWN,Color.BLACK),new Figure(FigureType.PAWN,Color.BLACK),new Figure(FigureType.PAWN,Color.BLACK),-2,-2,-2],
              [-2,-2,-2,-1,-1,-1,-1,-1,-1,-1,-1,-2,-2,-2],
              [new Figure(FigureType.ROOK,Color.RED),new Figure(FigureType.PAWN,Color.RED),-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,new Figure(FigureType.PAWN,Color.GREEN),new Figure(FigureType.ROOK,Color.GREEN)],
-             [new Figure(FigureType.BISHOP,Color.RED),new Figure(FigureType.PAWN,Color.RED),-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,new Figure(FigureType.PAWN,Color.GREEN),new Figure(FigureType.BISHOP,Color.GREEN)],
-             [new Figure(FigureType.KNIGHT,Color.RED),new Figure(FigureType.PAWN,Color.RED),-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,new Figure(FigureType.PAWN,Color.GREEN),new Figure(FigureType.KNIGHT,Color.GREEN)],
-             [new Figure(FigureType.QUEEN,Color.RED),new Figure(FigureType.PAWN,Color.RED),-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,new Figure(FigureType.PAWN,Color.GREEN),new Figure(FigureType.QUEEN,Color.GREEN)],
-             [new Figure(FigureType.KING,Color.RED),new Figure(FigureType.PAWN,Color.RED),-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,new Figure(FigureType.PAWN,Color.GREEN),new Figure(FigureType.KING,Color.GREEN)],
              [new Figure(FigureType.KNIGHT,Color.RED),new Figure(FigureType.PAWN,Color.RED),-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,new Figure(FigureType.PAWN,Color.GREEN),new Figure(FigureType.KNIGHT,Color.GREEN)],
              [new Figure(FigureType.BISHOP,Color.RED),new Figure(FigureType.PAWN,Color.RED),-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,new Figure(FigureType.PAWN,Color.GREEN),new Figure(FigureType.BISHOP,Color.GREEN)],
+             [new Figure(FigureType.KING,Color.RED),new Figure(FigureType.PAWN,Color.RED),-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,new Figure(FigureType.PAWN,Color.GREEN),new Figure(FigureType.QUEEN,Color.GREEN)],
+             [new Figure(FigureType.QUEEN,Color.RED),new Figure(FigureType.PAWN,Color.RED),-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,new Figure(FigureType.PAWN,Color.GREEN),new Figure(FigureType.KING,Color.GREEN)],
+             [new Figure(FigureType.BISHOP,Color.RED),new Figure(FigureType.PAWN,Color.RED),-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,new Figure(FigureType.PAWN,Color.GREEN),new Figure(FigureType.BISHOP,Color.GREEN)],
+             [new Figure(FigureType.KNIGHT,Color.RED),new Figure(FigureType.PAWN,Color.RED),-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,new Figure(FigureType.PAWN,Color.GREEN),new Figure(FigureType.KNIGHT,Color.GREEN)],
              [new Figure(FigureType.ROOK,Color.RED),new Figure(FigureType.PAWN,Color.RED),-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,new Figure(FigureType.PAWN,Color.GREEN),new Figure(FigureType.ROOK,Color.GREEN)],
              [-2,-2,-2,-1,-1,-1,-1,-1,-1,-1,-1,-2,-2,-2],
              [-2,-2,-2,new Figure(FigureType.PAWN,Color.WHITE),new Figure(FigureType.PAWN,Color.WHITE),new Figure(FigureType.PAWN,Color.WHITE),new Figure(FigureType.PAWN,Color.WHITE),new Figure(FigureType.PAWN,Color.WHITE),new Figure(FigureType.PAWN,Color.WHITE),new Figure(FigureType.PAWN,Color.WHITE),new Figure(FigureType.PAWN,Color.WHITE),-2,-2,-2],
-             [-2,-2,-2,new Figure(FigureType.ROOK,Color.WHITE),new Figure(FigureType.BISHOP,Color.WHITE),new Figure(FigureType.KNIGHT,Color.WHITE),new Figure(FigureType.KING,Color.WHITE),new Figure(FigureType.QUEEN,Color.WHITE),new Figure(FigureType.KNIGHT,Color.WHITE),new Figure(FigureType.BISHOP,Color.WHITE),new Figure(FigureType.ROOK,Color.WHITE),-2,-2,-2]];
+             [-2,-2,-2,new Figure(FigureType.ROOK,Color.WHITE),new Figure(FigureType.KNIGHT,Color.WHITE),new Figure(FigureType.BISHOP,Color.WHITE),new Figure(FigureType.QUEEN,Color.WHITE),new Figure(FigureType.KING,Color.WHITE),new Figure(FigureType.BISHOP,Color.WHITE),new Figure(FigureType.KNIGHT,Color.WHITE),new Figure(FigureType.ROOK,Color.WHITE),-2,-2,-2]];
 
+function setPosition(pos, figureID){
+    figureList[figureID].setPosition(pos.x, pos.y);
+    stage.draw();
+}
 
-$(function() {
+function removeFigure(index){
+    figureList[index].remove(); 
+    stage.draw();
+}
+
+$(document).ready(function () {
+    socket = io.connect('http://chess.flo-lan.com');
+    socket.on('message', function(msg){
+        $clientCounter.html(msg.clients);
+    });
+    socket.on('setPosition',setPosition);
+    socket.on('removeFigure',removeFigure);
+
+    $clientCounter = $('#client_count');
+         
     pieces = new Image();
     pieces.onload = function () {
         drawBoard();
     }    
     pieces.src = 'img/figures.png';
-});
+ });
+
 
 //draw Board on load
 function drawBoard() {
@@ -40,11 +59,13 @@ function drawBoard() {
     stage.on('click', function(e) {
         boardClicked(e);
     });
+
+    figureList = new Array();
     //board tiles
     boardLayer = new Kinetic.Layer(); //background layer for the chessboard
-    figureLayer = new Kinetic.Layer(); //layer for figures
     moveLayer = new Kinetic.Layer(); //where the figures can go to
     moveLayer.setOpacity(0.5);
+    figureLayer = new Kinetic.Layer(); //layer for figures
 
     for(var y = 0 ; y < 14 ; y++){
         for(var x = 0 ; x < 14 ; x++){
@@ -58,7 +79,7 @@ function drawBoard() {
                     height: TILE_SIZE,
                     fill: getBoardColor(x,y),
                     stroke: 'black',
-        strokeWidth: 1,
+                    strokeWidth: 1,
                 });
                 boardLayer.add(rect);
                 if(board[y][x] != -1){
@@ -71,8 +92,8 @@ function drawBoard() {
         }       
     }
     stage.add(boardLayer);
-    stage.add(figureLayer);
     stage.add(moveLayer);
+    stage.add(figureLayer);
 }
 
 //draw single figure with canvas
@@ -88,11 +109,32 @@ function drawFigure(x,y) {
         crop: {x: figurePos.x,y: figurePos.y,width: TILE_SIZE,height: TILE_SIZE}
     });
     figureLayer.add(figure);
+    figureList.push(figure);
+
+    figure.on("dragend", function() {
+        var pos = figure.getPosition();
+        var tilePos = getTileFromPos(pos.x,pos.y);
+        var newPosX = tilePos.x * TILE_SIZE;
+        var newPosY = tilePos.y * TILE_SIZE;
+        
+        
+        for(var i = 0; i< figureList.length; i++){
+            //look if antother figure is on the dopped tile
+            if(figureList[i].getPosition().x == newPosX && figureList[i].getPosition().y == newPosY){
+                //ignore dragged figure
+                if(figureList[i] != figure){
+                   socket.emit('sendRemoveFigure',i);
+                } 
+            }
+        }
+        var figureID = figureList.indexOf(figure);
+        socket.emit('sendPosition',{x:newPosX,y:newPosY},figureID);
+        stage.draw();
+    });
 }
 
 //canvas mousedown event
 function boardClicked(e) {
-    console.log(e.offsetX);
     tilePos = getTileFromPos(e.offsetX, e.offsetY);
     if(clickIsLegal(tilePos.x, tilePos.y)){
         var possibleMoves = board[tilePos.y][tilePos.x].possibleMoves();
@@ -103,16 +145,17 @@ function boardClicked(e) {
             height: TILE_SIZE,
             fill: 'red'
         });
+        moveLayer.removeChildren();
         moveLayer.add(rect);
-        stage.add(moveLayer);
+        stage.draw();
     }
 }
 
 //get tile coordinates from total coordinates
 function getTileFromPos(x, y) {
     var position = {
-        'x': parseInt(x / TILE_SIZE),
-        'y': parseInt(y / TILE_SIZE)
+        'x': Math.round(x / TILE_SIZE),
+        'y': Math.round(y / TILE_SIZE)
     };
     return position;
 }
